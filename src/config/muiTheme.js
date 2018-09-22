03b9c91d426eeaf7911a594ea0a6872de2373112
aemc <aemlcruz@gmail.com>
2018-09-22 14:31:26 -0400

    Added config file

---
+++ src/config/muiTheme.js
@@ -0,0 +1,13 @@
+import { createMuiTheme } from '@material-ui/core/styles/index';
+
+export const theme = createMuiTheme({
+  palette: {
+    mainaccent: '#4A90E2',
+    darkaccent: '#3167A6',
+    lightaccent: '#AAD0FC',
+    alertaccent: '#42C4FC',
+    darkgrey: '#313645',
+    mediumgrey: '#40485A',
+    lightgrey: '#ECEFF1'
+  }
+});
--- src/index.js
+++ src/index.js
@@ -4,5 +4,12 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider';
+import { theme } from './config/muiTheme';
+
+ReactDOM.render(
+  <MuiThemeProvider theme={theme}>
+    <App />
+  </MuiThemeProvider>
+  , document.getElementById('root'));
 registerServiceWorker();
